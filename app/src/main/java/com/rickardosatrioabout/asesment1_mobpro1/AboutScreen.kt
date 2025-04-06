cdcdd0b15ae5dcfe21370ac51198e89071b22de5
Rickardo Satrio <rickardosatrio@gmail.com>
2025-04-06 22:03:12 +0700

    manambahkan gambar di About screen

--- app/src/main/java/com/rickardosatrioabout/asesment1_mobpro1/AboutScreen.kt
+++ app/src/main/java/com/rickardosatrioabout/asesment1_mobpro1/AboutScreen.kt
@@ -1,7 +1,11 @@
 package com.rickardosatrioabout.asesment1_mobpro1
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,7 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +34,7 @@ fun AboutScreen(navController: NavController) {
         topBar = {
             TopAppBar(
                 navigationIcon = {
-                    IconButton(onClick = {navController.popBackStack()}) {
+                    IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = stringResource(R.string.back),
@@ -46,10 +52,24 @@ fun AboutScreen(navController: NavController) {
             )
         }
     ) { innerPadding ->
-        Text(
-            text = stringResource(R.string.historyText),
-            modifier = Modifier.padding(innerPadding).padding(16.dp)
-        )
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.kalkulator),
+                contentDescription = stringResource(R.string.kalkulator_image_desc),
+                        modifier = Modifier
+                    .size(250.dp)
+                    .padding(bottom = 24.dp)
+            )
+            Text(
+                text = stringResource(R.string.historyText)
+            )
+        }
     }
 }
 
